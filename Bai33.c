b6d728be204fce5b61462924fcc6050a6cf51a61
NexToue <furidom4everalone@gmail.com>
2024-12-13 13:26:37 +0700

    Update Bai33.c

--- Bai33.c
+++ Bai33.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+void timstg( int a, int b, int c) { if (a != b && b != c && a != c){
+        if ((a>b && a<c) || (a<b && a>c))
+        printf("a la trung gian");
+        else
+        { if ((c>b&&c<a) ||(c>a && c<b))
+        printf ("c la trung gian");
+        else if ((b>a&&b<c) ||(b>c && b<c)) printf("b la trung gian");
+        }
+        } else printf("Khong co so trung gian hoac a, b, c khong hop le");
+        }
 main()
 {
-	int a, b, c;
-	printf("Nhap so a:");
-	scanf("%d", &a);
-	printf("Nhap so b:");
-	scanf("%d", &b);
-	printf("Nhap so c:");
-	scanf("%d", &c);
-	if (a != b && b != c && a != c){
-	if ((a>b && a<c) || (a<b && a>c))
-	printf("a la trung gian");
-	else
-	{ if ((c>b&&c<a) ||(c>a && c<b))
-	printf ("c la trung gian");
-	else if ((b>a&&b<c) ||(b>c && b<c)) printf("b la trung gian");
-	}
-	} else printf("Khong co so trung gian hoac a, b, c khong hop le");
-	}
+        int a, b, c;
+        printf("Nhap so a:");
+        scanf("%d", &a);
+        printf("Nhap so b:");
+        scanf("%d", &b);
+        printf("Nhap so c:");
+        scanf("%d", &c);
+       timstg(a, b, c);
+      }
