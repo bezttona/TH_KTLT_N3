870b45c81fbfa2452d155a79831c3f554a6ac7bf
ayafullcrit <nguyendangphong2k6@gmail.com>
2024-12-13 11:32:13 +0700

    Update c86.c

--- c86.c
+++ c86.c
@@ -1,26 +1,32 @@
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
-
-int main(){
-	char s1[100],s2[100];
-	gets(s1);gets(s2);
-	if (strlen(s1)!= strlen(s2)) printf("Hai chuoi %s va %s khong la anagram ",s1,s2);
-	else{
+#include<stdbool.h>
+bool is_anagram(char s1[],char s2[])
+{
+	if (strlen(s1)!= strlen(s2)) return 0;
 	int b[400],c[400];
-	int kt=1;
-	for(int i=65;i<=122;i++){
-	b[i]=0;c[i]=0;
+	int i;
+	for(i=0;i<400;i++)
+	{
+		b[i]=0;c[i]=0;
 	}
-	for(int i=0;i<=strlen(s1);i++)
-	b[int(s1[i])]+=1;
-	for(int i=0;i<=strlen(s2);i++)
-	c[int(s2[i])]+=1;
-	if ((b[i]==0 && c[i]!=0) || (b[i]!=0 && c[i]==0)){
-		kt=0;break;
+	for(i=0;i<strlen(s1);i++)
+	b[s1[i]]++;
+	for(i=0;i<strlen(s2);i++)
+	c[s2[i]]++;
+	for(i=0;i<strlen(s2);i++)
+	{
+		if(b[s1[i]]!=c[s1[i]]) return 0;
 	}
-
-	if(kt) printf("Hai chuoi %s va %s la anagram ",s1,s2);else printf("Hai chuoi %s va %s khong la anagram ",s1,s2);
-	getch();
+	return 1;	
 }
+main()
+{
+	char s1[100],s2[100];
+	gets(s1);
+	gets(s2);
+	if(is_anagram(s1,s2)) printf("Hai chuoi %s va %s la anagram ",s1,s2);
+	else printf("Hai chuoi %s va %s khong la anagram ",s1,s2);
+	return ;
 }
