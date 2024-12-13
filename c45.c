760b0d0cb415621b776d5db9c5bd90c6e3e0099f
ayafullcrit <nguyendangphong2k6@gmail.com>
2024-12-13 11:36:55 +0700

    Update c45.c

--- c45.c
+++ c45.c
@@ -3,7 +3,8 @@
 #include<math.h>
 double tong(float x,int n){
 	double sum=1+x;float a=x;
-	for(int i=2;i<=n;i++){
+	int i;
+	for(i=2;i<=n;i++){
 	a=a*x*(i-1)/i;sum+=a;
 	}
 	return sum;		
@@ -13,5 +14,4 @@ int main(){
 	int n;float x;
 	scanf("%f %d",&x,&n);
 	printf("Tong S(n)= %.5f",tong(x,n));
-	getch();
 }
