5499e4f0aebca96675203351360185f0f6b03a5e
NexToue <furidom4everalone@gmail.com>
2024-12-13 13:15:27 +0700

    Update Bai1.c

--- Bai1.c
+++ Bai1.c
@@ -3,20 +3,20 @@
 void kiemtra3canh(int a, int b, int c) {
     if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
         if (a == b && b == c) {
-            printf("Tam giac deu\n");
+            printf("Tam giac deu");
         } else if (a == b || b == c || c == a) {
             if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
-                printf("Tam giac vuong can\n");
+                printf("Tam giac vuong can");
             } else {
-                printf("Tam giac can\n");
+                printf("Tam giac can");
             }
         } else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
-            printf("Tam giac vuong\n");
+            printf("Tam giac vuong");
         } else {
-            printf("Tam giac binh thuong\n");
+            printf("Tam giac binh thuong");
         }
     } else {
-        printf("Khong phai tam giac\n");
+        printf("Khong phai tam giac");
     }
 }
 main ()
