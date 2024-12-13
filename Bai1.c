926f0571dd8a3d3e1d23761979db563fd64868bf
NexToue <furidom4everalone@gmail.com>
2024-12-13 13:14:02 +0700

    Update Bai1.c

--- Bai1.c
+++ Bai1.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include  <math.h>
+void kiemtra3canh(int a, int b, int c) {
+    if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
+        if (a == b && b == c) {
+            printf("Tam giac deu\n");
+        } else if (a == b || b == c || c == a) {
+            if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
+                printf("Tam giac vuong can\n");
+            } else {
+                printf("Tam giac can\n");
+            }
+        } else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
+            printf("Tam giac vuong\n");
+        } else {
+            printf("Tam giac binh thuong\n");
+        }
+    } else {
+        printf("Khong phai tam giac\n");
+    }
+}
 main ()
 {
     int a,b,c;
     scanf("%d %d %d", &a, &b, &c);
-    if (a>0 && b>0 && c>0)
-    {
-        if (a+b>c && a+c>b && b+c>a)
-     {
-    if (a==b && b==c && c==a) printf("Tam giac deu");
-    else{
-    if (a==b || b == c || c == a) 
-	{ if (a*a+ b*b==c*c || b*b+c*c==a*a|| a*a+c*c==b*b) printf ("Tam giac vuong can");
-	else printf("Tam giac can"); }
-	else if(a*a+ b*b==c*c || b*b+c*c==a*a|| a*a+c*c==b*b) printf ("Tam giac vuong");
-	else printf("Tam giac binh thuong");
-	}}
-        else printf("Khong phai tam giac");
-    }
-    else printf("Khong phai tam giac");
+    kiemtra3canh(a, b, c);
 }
