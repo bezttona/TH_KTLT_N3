32dcdee43a228efbe6e10e50ab95727f1b865788
ayafullcrit <nguyendangphong2k6@gmail.com>
2024-12-13 11:33:57 +0700

    Update c6.c

--- c6.c
+++ c6.c
@@ -3,22 +3,22 @@
 #include<math.h>
   double  tich(float a,unsigned int n){
   double  s=1;
-	for(int i=0;i<=n-1;i++){
+  int i;
+	for(i=0;i<=n-1;i++){
 	s=s*(a+i);
 	}
 	return s;		
 }
  double  tong(float a,unsigned int n){
   double  s=1/a,b=1/a;
-	for(int i=1;i<=n;i++){
+  int i;
+	for(i=1;i<=n;i++){
 	b=b*1/(a+i);s+=b;
 	}
 	return s;		
 }
-int main(){
+void main(){
 	unsigned int n;float a;
 	scanf("%f %d",&a,&n);
-	printf("a. Ket qua: %.5lf \n b. Ket qua: %.5lf",tich(a,n),tong(a,n));
-	
-	getch();
+	printf("a. Ket qua: %.5lf \nb. Ket qua: %.5lf",tich(a,n),tong(a,n));
 }
